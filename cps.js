779d35d606049fb61626bc4ff91e625bbb528c2d
Rick Gao 高　奇 <H36360@evaair.com>
2020-09-30 15:37:59 -0700

    add pdf file merge feature; Final result can be create in seperate single pdf file; BTW, I'm so HAPPY:)

--- cps.js
+++ cps.js
@@ -1,13 +1,20 @@
 const fs = require("fs");
 const pdf = require("pdf-extraction");
 const pdfFiles = fs.readdirSync("./pdf").map((el) => `./pdf/${el}`);
-const cd = [];
+const {jsPDF} = require("jspdf")
+
+const merge = require('easy-pdf-merge');
+
+
+
+var cd = [];
 console.log(pdfFiles);
 
-function accountChangeSum() {
-  const result = cd.reduce((r, a) => a.map((b, i) => (r[i] || 0) + b), []);;
-  result.map(el=>el/100)
-  console.log(result.map(el=>el/100));
+function accountChangeSum(arr) {
+  const result = arr.reduce((r, a) => a.map((b, i) => (r[i] || 0) + b), []);;
+  cd = result.map(el=>(el/100).toString())
+  console.log(cd)
+  // console.log(result.map(el=>(el/100).toString()));
 }
 
 function getOriginalAmount(){
@@ -57,7 +64,38 @@ async function getDCSummary() {
     });
   }
   await accountChangeSum(cd);
+  await createResultPDF()
+  await mergePDF()
 }
 
 getDCSummary();
-// getOriginalAmount()
+
+function createResultPDF(){
+
+  const doc = new jsPDF({
+    orientation: "landscape",
+    unit: "in",
+    format: [11, 8.5]
+  });
+  
+  // doc.text(cd[0], 9.5, 2.875);
+  doc.text(cd[1], 9.5, 2.875);
+  doc.text(cd[2], 9.5, 3.125);
+  doc.text(cd[3], 9.5, 3.75);
+  doc.text(cd[4], 9.5, 4);
+  doc.text(cd[5], 9.5, 4.63);
+  doc.save("./pdf/forReview.pdf");
+
+
+}
+
+function mergePDF(){
+
+const pdfPath = fs.readdirSync('./pdf').map(el=>`./pdf/${el}`)
+merge(pdfPath, './pdf/forPrint.pdf', function (err) {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Success')
+});
+}

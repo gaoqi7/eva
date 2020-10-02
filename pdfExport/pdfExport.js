f796428a35be518afdb64f92ac51daedfde149ce
Rick Gao 高　奇 <H36360@evaair.com>
2020-10-02 15:35:18 -0700

    Now I can REAL EASILY to export pdf files to folder

---
+++ pdfExport/pdfExport.js
@@ -0,0 +1,37 @@
+const robot = require("robotjs");
+const sleep = require('sleep')
+const toPath = 'C:\\Users\\H36360\\Documents\\eva\\pdf'
+// var mouse = robot.getMousePos();
+// console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y)
+const startPoint = [93,299]
+function pdfExport(times){
+    sleep.sleep(5)
+    let originalY = startPoint[1]
+for (i = 0;i<times;i++){
+    originalY = startPoint[1]+ i * 33
+    const pdfFileName = `\\${times - i}.pdf`
+robot.moveMouse(93,originalY)
+robot.mouseClick()
+sleep.sleep(2)
+
+robot.moveMouse(555,115)
+robot.mouseClick()
+robot.typeString('a')
+robot.keyTap("tab")
+robot.keyTap("tab")
+robot.keyTap("tab")
+robot.keyTap("tab")
+robot.typeString(`${toPath}${pdfFileName}`)
+robot.keyTap('tab')
+robot.keyTap('enter')
+sleep.msleep(500)
+
+robot.keyTap('tab')
+robot.keyTap('enter')
+sleep.msleep(500)
+}
+
+}
+
+
+pdfExport(4)

23be0106c10a4d840632e20e38eac2a13ee032b9
Rick Gao 高　奇 <H36360@evaair.com>
2020-10-19 16:01:59 -0700

    General Voucher Can be keyed in automatically Now, I want to have a frontend

--- gvInput/gv.js
+++ gvInput/gv.js
@@ -11,63 +11,59 @@ var ws
 
 async function getData(){
   await workbook.xlsx.readFile('ogfn.xlsx')
-  ws =await  workbook.getWorksheet('ofgn')
+  ws =await  workbook.getWorksheet('ogfn')
 console.log(typeof ws)
+sleep.sleep(6)
 cool()
 }
 
 getData()
-
+console.log(linesNeedKeyIn)
+console.log(typeof linesNeedKeyIn)
 // getData()
-// sleep.sleep(5)
 function cool(){
 
 
-for (r = 2; r<linesNeedKeyIn+2; r++){
+for (r = 2; r<parseInt(linesNeedKeyIn)+2; r++){
   for(c = 2; c<15; c++){
     
-    function ha(){
-    console.log(r)
-    console.log(c)
-      let cRow = ws.getRow(r)
-      let b = cRow.getCell(c).value
-      // let a =  ws.getCell(`B2`).value
-      // console.log(a)
-    console.log("b",b)
-  }
+  //   function ha(){
+  //   console.log(r)
+  //   console.log(c)
+  //     let cRow = ws.getRow(r)
+  //     let b = cRow.getCell(c).value
+  //     // let a =  ws.getCell(`B2`).value
+  //     // console.log(a)
+  //   console.log("b",b)
+  // }
     
-    ha()
+  //   ha()
 
 
 
 
-    // async function getData(){
       
-      //   await workbook.xlsx.readFile('ogfn.xlsx')
-//   const ws = workbook.getWorksheet('ogfn')
-// console.log("haha")
-// console.log(typeof ws)
-// let currentRow = ws.getRow(r)
-// // console.log(currentRow)
-// let data = currentRow.getCell(c).value
-// console.log(data)
-// if(c !== 14){
-// if(data){
+let currentRow = ws.getRow(r)
+// console.log(currentRow)
+let data = currentRow.getCell(c).value
+console.log(data)
+if(c !== 14){
+if(data){
 
-//   robot.typeStringDelayed(data,80)
-// }
-//   robot.keyTap('tab')
-// }else{    
-//   robot.typeStringDelayed(data,80)
-//   robot.keyTap('tab')
-//   robot.keyTap('tab')
-//   robot.keyTap('tab')
-//   robot.keyTap('tab')
+  // robot.typeStringDelayed(data,200)
+  robot.typeString(data)
+}
+  robot.keyTap('tab')
+}else{    
+  // robot.typeStringDelayed(data,200)
+  robot.typeString(data)
+  robot.keyTap('tab')
+  robot.keyTap('tab')
+  robot.keyTap('tab')
+  robot.keyTap('tab')
 
-// }
-// // currentRow.getCell(1).value = 'Done'
-// }
-// getData()
+}
+// currentRow.getCell(1).value = 'Done'
      
   }  
 }

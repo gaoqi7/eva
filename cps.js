5c368ee940a2b569f0f65e68626ab8049fc4993e
Rick Gao 高　奇 <H36360@evaair.com>
2020-10-21 16:32:11 -0700

    finally, Finish test on file server , works very well, btw, It is not worth to fight with a ...

--- cps.js
+++ cps.js
@@ -1,67 +1,65 @@
 //***************************** */
 
-// node cps.js y 
+// node cps.js y
 // node cps.js n
 
 //***************************** */
 const fs = require("fs");
-const fse = require('fs-extra')
+const fse = require("fs-extra");
 // The package below extraction the text from pdf.
 const pdf = require("pdf-extraction");
 // All CPS daily report store in pdf folder
 const pdfFiles = fs.readdirSync("./pdf").map((el) => `./pdf/${el}`);
-const totalPdfFiles = pdfFiles.length
-console.log(totalPdfFiles)
+const totalPdfFiles = pdfFiles.length;
+console.log(totalPdfFiles);
 
-const isWrite = process.argv[2]
+const isWrite = process.argv[2];
 
 //Use the package below for merging all pdf files. Make it easy to print
-const merge = require('easy-pdf-merge');
+const merge = require("easy-pdf-merge");
 
-const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
+const { PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 const { cpus } = require("os");
 // Empty array, used to store the calcuated result for total Credit(c) and total Debit(d)
 var cd = [];
-// 
-let unCashOutCheckAmount = 0
+//
+let unCashOutCheckAmount = 0;
 console.log(pdfFiles);
 // use this function to accumulate day's amount change for each account
 // list of array, the values have the same index will be added together and store in array cd
 function accountChangeSum(arr) {
-  const result = arr.reduce((r, a) => a.map((b, i) => (r[i] || 0) + b), []);;
-  cd = result.map(el=>(el/100).toString())
-  console.log(cd)
+  const result = arr.reduce((r, a) => a.map((b, i) => (r[i] || 0) + b), []);
+  cd = result.map((el) => (el / 100).toString());
+  console.log(cd);
 }
 
 // Get the Start Amount
-function getOriginalAmount(){
+function getOriginalAmount() {
   //oAS means original Account Statement
-  let oAS = []
-  let dataBuffer = fs.readFileSync('./pdf/1.pdf')
-  pdf(dataBuffer).then(data=>{
+  let oAS = [];
+  let dataBuffer = fs.readFileSync("./pdf/1.pdf");
+  pdf(dataBuffer).then((data) => {
     let bss = data.text
-        .split("\n")
-        .filter((el) => el.endsWith("USD"))
-        .map((el) => el.replace(/,/g, ""));
-        console.log(bss)
-     bss.map(ele=>{
-       const oA = parseInt(parseFloat(ele.trim().split("  ")[3].split(' ')[0])*100)
-      //  console.log(oA) 
-       oAS.push(oA)
-     })
-     cd.push(oAS)
-     console.log("this is original cd")
-     console.log(cd)
-  })
+      .split("\n")
+      .filter((el) => el.endsWith("USD"))
+      .map((el) => el.replace(/,/g, ""));
+    console.log(bss);
+    bss.map((ele) => {
+      const oA = parseInt(
+        parseFloat(ele.trim().split("  ")[3].split(" ")[0]) * 100
+      );
+      //  console.log(oA)
+      oAS.push(oA);
+    });
+    cd.push(oAS);
+    console.log("this is original cd");
+    console.log(cd);
+  });
 }
-
-
-
 
 //Deal with the raw data and get the data I need for CPS, which is Debit amount and Credit amount
 async function getDCSummary() {
-
-  await getOriginalAmount()
+  await getOriginalAmount();
   for (const pdfFile of pdfFiles) {
     let dataBuffer = fs.readFileSync(pdfFile);
     await pdf(dataBuffer).then((data) => {
@@ -87,8 +85,6 @@ async function getDCSummary() {
   await mergePDF();
 }
 
-
-
 // function createResultPDF(){
 
 //   const doc = new jsPDF({
@@ -96,7 +92,7 @@ async function getDCSummary() {
 //     unit: "in",
 //     format: [11, 8.5]
 //   });
-  
+
 //   // doc.text(cd[0], 9.5, 2.875);
 //   doc.text(cd[1], 9.5, 2.875);
 //   doc.text(cd[2], 9.5, 3.125);
@@ -105,45 +101,42 @@ async function getDCSummary() {
 //   doc.text(cd[5], 9.5, 4.63);
 //   doc.save("./pdf/forReview.pdf");
 
-
 // }
 
-function mergePDF(){
-
-const pdfPath =fs.readdirSync('./pdf').map(el=>`./pdf/${el}`)
-merge(pdfPath, './pdf/forPrint.pdf', function (err) {
+function mergePDF() {
+  const pdfPath = fs.readdirSync("./pdf").map((el) => `./pdf/${el}`);
+  merge(pdfPath, "./pdf/forPrint.pdf", function (err) {
     if (err) {
-        return console.log(err)
+      return console.log(err);
     }
 
-for (i = 1;i <= totalPdfFiles;i++){
-fse.removeSync(`./pdf/${i}.pdf`)
-}
-
-// if process.argv[2] doesn't equal Y, means this CPS report will not be showed to Manager, that is why we can still modify.
-
-    console.log('Success')
-    if (isWrite !== "y"){
-      modifyLastPage()
+    for (i = 1; i <= totalPdfFiles; i++) {
+      fse.removeSync(`./pdf/${i}.pdf`);
     }
-});
+
+    // if process.argv[2] doesn't equal Y, means this CPS report will not be showed to Manager, that is why we can still modify.
+
+    console.log("Success");
+    if (isWrite !== "y") {
+      modifyLastPage();
+    }
+  });
 }
 
-
-async function modifyLastPage(){
-  const existingPdfBytes = await fs.readFileSync('./pdf/forPrint.pdf')
+async function modifyLastPage() {
+  const existingPdfBytes = await fs.readFileSync("./pdf/forPrint.pdf");
   // console.log(existingPdfBytes)
-  const pdfDoc = await PDFDocument.load(existingPdfBytes)
-  
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
   // Embed the Helvetica font
-  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
-  const pages = pdfDoc.getPages()
-  const firstPage = pages[totalPdfFiles-1]
-  
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[totalPdfFiles - 1];
+
   // Get the width and height of the first page
-  const { width, height } = firstPage.getSize()
-  console.log(width)
-  console.log(height)
+  const { width, height } = firstPage.getSize();
+  console.log(width);
+  console.log(height);
   // Draw a string of text diagonally across the first page
   firstPage.drawText(cd[0], {
     x: 760,
@@ -151,68 +144,61 @@ async function modifyLastPage(){
     size: 12,
     font: helveticaFont,
     color: rgb(0.95, 0.1, 0.1),
-  })
+  });
   firstPage.drawText(cd[1], {
     x: 760,
-  y: 403,
-  size: 12,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
-firstPage.drawText(cd[2], {
-  x: 760,
-  y: 388,
-  size: 12,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
-firstPage.drawText(cd[3], {
-  x: 760,
-  y: 335,
-  size: 12,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
-firstPage.drawText(cd[4], {
-  x: 760,
-  y: 320,
-  size: 12,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
-firstPage.drawText(cd[5], {
-  x: 760,
-  y: 265,
-  size: 12,
-  font: helveticaFont,
-  color: rgb(0.95, 0.1, 0.1),
-})
+    y: 403,
+    size: 12,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
+  firstPage.drawText(cd[2], {
+    x: 760,
+    y: 388,
+    size: 12,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
+  firstPage.drawText(cd[3], {
+    x: 760,
+    y: 335,
+    size: 12,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
+  firstPage.drawText(cd[4], {
+    x: 760,
+    y: 320,
+    size: 12,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
+  firstPage.drawText(cd[5], {
+    x: 760,
+    y: 265,
+    size: 12,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
 
-
-const pdfBytes = await pdfDoc.save()
-await fs.writeFileSync('./pdf/forPrint.pdf', pdfBytes)
-
-
+  const pdfBytes = await pdfDoc.save();
+  await fs.writeFileSync("./pdf/forPrint.pdf", pdfBytes);
 }
 
-// This part is used for parse the r710 report. 
-function r710(){
-  let r710Buffer = fs.readFileSync('./r710/710.pdf')
-  pdf(r710Buffer).then(data => {
+// This part is used for parse the r710 report.
+function r710() {
+  let r710Buffer = fs.readFileSync("./r710/710.pdf");
+  pdf(r710Buffer).then((data) => {
     // data.text.split('\n')
-    const textArr = data.text.split('\n')
+    const textArr = data.text.split("\n");
     // console.log(textArr[textArr.length-2])
-    unCashOutCheckAmount =  parseFloat(textArr[textArr.length-2].replace(',',''))
-    console.log(unCashOutCheckAmount)
-  })
+    unCashOutCheckAmount = parseFloat(
+      textArr[textArr.length - 2].replace(",", "")
+    );
+    console.log(unCashOutCheckAmount);
+  });
 }
 
 // r710()
 
-
-
 getDCSummary();
-
-
-
-

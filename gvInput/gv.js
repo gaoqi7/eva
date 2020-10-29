00ac2dd9b9078e2c584f3172bf2dee2b6fcb26b8
Rick Gao 高　奇 <H36360@evaair.com>
2020-10-29 16:50:28 -0700

    adjust the gvInput function. Now, I can free copy paste in excel because I can expand the description columns

--- gvInput/gv.js
+++ gvInput/gv.js
@@ -2,8 +2,9 @@ const robot = require("robotjs");
 const sleep = require("sleep");
 const Excel = require("exceljs");
 const workbook = new Excel.Workbook();
-
-const linesNeedKeyIn = process.argv[2];
+const workbookName = process.argv[2];
+const worksheetName = process.argv[3];
+const linesNeedKeyIn = process.argv[4];
 
 const seRow = [2, 16];
 const seCol = [2, 14];
@@ -11,9 +12,9 @@ var ws;
 
 async function getData() {
   // await workbook.xlsx.readFile('ogfn.xlsx')
-  await workbook.xlsx.readFile("yyyymmdd.xlsx");
+  await workbook.xlsx.readFile(`${workbookName}.xlsx`);
 
-  ws = await workbook.getWorksheet("ymd");
+  ws = await workbook.getWorksheet(`${worksheetName}`);
   console.log(typeof ws);
   sleep.sleep(6);
   cool();
@@ -38,6 +39,21 @@ function cool() {
         }
         robot.keyTap("tab");
       } else {
+        // let data15 = currentRow.getCell(15).value;
+        // let data16 = currentRow.getCell(16).value;
+        // let data17 = currentRow.getCell(17).value;
+        // let data18 = currentRow.getCell(18).value;
+        // data = data + data15 + data16 + data17 + data18;
+        function dataParser(n) {
+          if (currentRow.getCell(n).value) {
+            return currentRow.getCell(n).text;
+          } else {
+            return " ";
+          }
+        }
+        for (n = 15; n < 19; n++) {
+          data += dataParser(n);
+        }
         // robot.typeStringDelayed(data,200)
         robot.typeString(data);
         robot.keyTap("tab");

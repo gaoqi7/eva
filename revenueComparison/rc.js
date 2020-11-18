2d113aa99640d91b55d5af7128d4d69e44ddc61a
Rick Gao 高　奇 <H36360@evaair.com>
2020-11-17 17:29:08 -0800

    it is just hard to modify cell content which is rich text

--- revenueComparison/rc.js
+++ revenueComparison/rc.js
@@ -5,16 +5,35 @@ async function getExcelReady() {
   workbook.eachSheet(function (worksheet, sheetId) {
     // total Row
     let ttlR = worksheet.rowCount;
-    for (i = 2; i < worksheet.rowCount; i++) {
-      // worksheet.getCell(`C${i}`).richText = worksheet.getCell(`B${i}`).richText;
-      console.log("did i change", worksheet.getCell(`C${i}`).value);
-      // if (i === 2) {
-      //   let a = worksheet.getCell(`C${i}`).richText.replace(/A/g, "B");
-      //   console.log(a);
-      //   worksheet.getCell(`C${i}`).richText = a;
-      // }
+
+    console.log("total row ", ttlR);
+    for (i = 2; i < ttlR - 1; i++) {
+      // console.log("rich text ", worksheet.getCell(`C${i}`).value.richText);
+      if (i === 2) {
+        worksheet.getCell("C2").value.richText[
+          worksheet.getCell(`C2`).value.richText.length - 1
+        ].text = "(B)";
+
+        let b_month = parseInt(
+          worksheet.getCell(`B2`).value.richText[0].text[0]
+        );
+        console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", 1 + b_month);
+        console.log(worksheet.getCell(`B2`).value.richText[0].text[0]);
+        if (worksheet.getCell(`B2`).value.richText[0].text[0].length === 1) {
+        }
+        // console.log(`10${fff.slice(-3)}`);
+        // fff = `${1 + b_month}${fff.slice(-(fff.length - 1))}`;
+
+        // console.log("hahahah", 1 + b_month);
+        // console.log("B22222222222222", worksheet.getCell(`B2`).value);
+      } else {
+        // console.log("B's text", worksheet.getCell(`B${i}`).text);
+
+        worksheet.getCell(`C${i}`).value = worksheet.getCell(`B${i}`).value;
+        // console.log("did i change", worksheet.getCell(`C${i}`).text);
+      }
     }
-    // return workbook.xlsx.writeFile("收入比較-202009.xlsx");
   });
+  return workbook.xlsx.writeFile("收入比較-202009.xlsx");
 }
 getExcelReady();

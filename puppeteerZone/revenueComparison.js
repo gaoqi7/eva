b25cb377dd44f20dbb8d96e98d72ccf9b61f8b4f
Rick Gao 高　奇 <H36360@evaair.com>
2020-11-20 16:25:25 -0800

    add new project which use  puppeteer to automate search account code from fs

---
+++ puppeteerZone/revenueComparison.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto("http://wbclsb04prod.evaair.com/hostfin/main.asp");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();

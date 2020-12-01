43e1085ae0b157063741327e01f09f2ec288f49d
Rick Gao 高　奇 <H36360@evaair.com>
2020-11-30 16:11:28 -0800

    minor modify

--- puppeteerZone/revenueComparison.js
+++ puppeteerZone/revenueComparison.js
@@ -5,6 +5,8 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto("http://wbclsb04prod.evaair.com/hostfin/main.asp");
+  await page.bringToFront();
+  // await page.keyboard.press(String.fromCharCode(13));
   await page.screenshot({ path: "example.png" });
 
   await browser.close();

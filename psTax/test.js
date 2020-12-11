eb4d4a0e400f7206c602ff45b355d1debb740369
Rick Gao 高　奇 <H36360@evaair.com>
2020-12-11 10:10:04 -0800

    what am i doing?

--- psTax/test.js
+++ psTax/test.js
@@ -1,17 +1,6 @@
 const fse = require("fs-extra");
 const pdf = require("pdf-extraction");
-
-let dataBuffer = fse.readFileSync("pp1.pdf");
-
-Array.prototype.chunk = function (chunk_size) {
-  if (!this.length) {
-    return [];
-  }
-
-  return [this.slice(0, chunk_size)].concat(
-    this.slice(chunk_size).chunk(chunk_size)
-  );
-};
+let dataBuffer = fse.readFileSync("pp.pdf");
 pdf(dataBuffer).then(function (data) {
   console.log(data.text);
   let rawArr = data.text.split("\n");
@@ -19,35 +8,37 @@ pdf(dataBuffer).then(function (data) {
   let tc;
   let container = [[]];
   // console.log(rawArr);
-  let temp = [];
+  // Three Cases
+  // 1. Employee ... FEDMEDCARE-ER {Amount} Employee
+  // 2. Employee ... FEDMEDCARE-ER {Amount} ... CASUI-ER {Amount} Employee
+  // 3. Employee ... FEDMEDCARE-ER {Amount} ... CASUI-ER ...(Because of new page) {Amount} Employee
   rawArr.forEach((el, i) => {
+    // V as Verify; Verify if it is case 3 - can't get amount because of header on new page.
     let v;
+
     if (el.startsWith("Employee:")) {
       container[counter] = [];
-      container[counter].push(el.slice(9));
+      container[counter].push(el);
+      //tc as temporary counter, used for collect employee's tax
       tc = counter;
+      // isNaN means is Not a Number
+      // Deal with the case 3.
       if (!isNaN(rawArr[i - 1]) && counter !== 0) {
         v = rawArr[i - 1];
-        if (
-          container[counter - 1].indexOf(v) === -1 &&
-          container[counter - 1].length < 3
-        ) {
+        if (container[counter - 1].indexOf(v) === -1) {
           container[counter - 1].push(v);
         }
       }
-
-      // console.log(tc);
+      // counter plus one for next employee; for current employee, use tc.
       counter++;
     }
+    // Sometime, new employee will have CASUI-ER.
     if (
       (el.startsWith("FEDMEDCARE-ER") || el.startsWith("CASUI-ER")) &&
       !isNaN(rawArr[i + 1])
     ) {
-      // console.log(rawArr[i + 1]);
       container[tc].push(rawArr[i + 1]);
     }
-
-    // console.log(container[tc]);
   });
   console.log(container);
 });
